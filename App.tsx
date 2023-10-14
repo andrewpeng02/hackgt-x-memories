3ea4a740277bbe284877a482202821efd66c94de
andrewpeng02 <andrewpeng02@gmail.com>
2023-10-13 21:22:35 -0400

    initial

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import RootNavigation from './navigation';
+
+export default function App() {
+  return (
+    <RootNavigation />
+  );
+}
